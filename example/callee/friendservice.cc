c5e10f6a50494e46c9ef082323523fe593323927
ggbond <chasir0401@gmail.com>
2026-05-12 16:32:19 +0800

    新增一个使用服务示例

---
+++ example/callee/friendservice.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "friend.pb.h"
+#include <vector>
+#include "mprpcapplication.h"
+#include "rpcprovider.h"
+
+class FriendService : public fixbug::FriendServiceRpc
+{
+public:
+    std::vector<std::string> GetFriendList(uint32_t user_id) {
+        // 这里可以根据user_id查询好友列表
+        // 假设我们查询到的好友列表如下
+        std::cout << "GetFriendList called with user_id: " << user_id << std::endl;
+        std::vector<std::string> vec;
+        vec.push_back("friend1");
+        vec.push_back("friend2");
+        vec.push_back("friend3");
+        return vec; 
+    }
+
+    void GetFriendList(::google::protobuf::RpcController* controller,
+                       const ::fixbug::GetFriendListRequest* request,
+                       ::fixbug::GetFriendListResponse* response,
+                       ::google::protobuf::Closure* done)
+    {
+        uint32_t user_id = request->id();
+        std::vector<std::string> friends = GetFriendList(user_id);
+        // 将好友列表添加到响应中
+        for (const auto& friend_name : friends) {
+            response->add_friends(friend_name);
+        }
+        response->mutable_result()->set_errcode(0);
+        response->mutable_result()->set_errmsg("");
+
+        done->Run();
+    }
+
+};
+
+int main(int argc, char** argv){
+    //调用框架初始化操作
+    MprpcApplication::Init(argc,argv);
+
+    //把FriendService对象发布到rpc节点上  让rpc框架来调用这个对象提供的GetFriendList方法
+    RpcProvider provider;
+    provider.NotifyService(new FriendService());
+
+    //启动rpc服务
+    provider.Run();
+    return 0;
+}
---
+++ example/caller/callfriendservice.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "mprpcapplication.h"
+#include "mprpcchannel.h"
+#include "friend.pb.h"
+int main(int argc, char** argv){
+    // 整个程序启动以后，想使用mprpc框架来享受rpc服务调用，一定要先调用框架的初始化函数（只初始化一次）
+    MprpcApplication::Init(argc, argv);
+
+    // FriendServiceRPC_Stub(::PROTOBUF_NAMESPACE_ID::RpcChannel* channel);
+    fixbug::FriendServiceRpc_Stub stub_(new MprpcChannel());
+
+    fixbug::GetFriendListRequest request;
+    request.set_id(100);
+    // rpc方法的响应
+    fixbug::GetFriendListResponse response;
+    stub_.GetFriendList(nullptr,&request,&response,nullptr);
+
+    // 一次rpc调用完成，读调用的结果
+    if(0 == response.result().errcode()){
+        std::cout << "rpc GetFriendList response success: " << response.success() << std::endl;
+        int size = response.friends_size();
+        for(int i = 0; i < size; ++i){
+            std::cout << "friend: " << response.friends(i) << std::endl;
+        }
+        
+    }else{
+        std::cout << "rpc GetFriendList response error" << response.result().errmsg() << std::endl;
+    }
+
+    return 0;
+}
